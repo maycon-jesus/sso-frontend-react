c92ba9d01f1996c527c807243ca272f4bc813c75
Maycon Jesus <mayconjesus07@gmail.com>
2022-09-10 21:47:00 -0300

    feat: open graph tags

--- src/components/utils/HeadUtil.tsx
+++ src/components/utils/HeadUtil.tsx
@@ -8,6 +8,18 @@ export function HeadUtil(props: Props) {
   return (
     <Head>
       <title>{props.title} - Maycon Jesus</title>
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={`${props.title} - Maycon Jesus SSO`} />
+      <meta property="og:url" content="https://sso.mayconjesus.dev" />
+      <meta
+        property="og:image"
+        content="https://sso-frontend-l527or7hl-maycon.vercel.app/assets/images/logo/og-image.png"
+      />
+      <meta
+        property="og:description"
+        content="Sistema de autenticação única para sites desenvolvidos por Maycon Jesus"
+      ></meta>
     </Head>
   );
 }
