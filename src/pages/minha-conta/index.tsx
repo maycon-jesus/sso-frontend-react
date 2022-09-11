6153ba2ff75f10600f2f33be73a9607394313c5c
Maycon Jesus <mayconjesus07@gmail.com>
2022-09-11 20:43:41 -0300

    fix: layout and head

--- src/pages/minha-conta/index.tsx
+++ src/pages/minha-conta/index.tsx
@@ -5,36 +5,40 @@ import { useRecoilValue } from "recoil";
 import { useAuthUserDataState } from "states/Auth";
 import { BasicUserDataForm } from "components/my-account/BasicUserDataForm";
 import styles from "./minha-conta.module.scss";
+import { HeadUtil } from "components/utils/HeadUtil";
 
 export default function Page() {
   const userData = useRecoilValue(useAuthUserDataState);
 
   return (
-    <LoggedAreaLayout>
-      <Title title="Minha conta"></Title>
-      <Container maxW="2xl">
-        <Flex width="full" flexDirection="column" alignItems="center">
-          <Flex width="full" justifyContent="center" alignItems="center">
-            <Avatar
-              size="xl"
-              name={`${userData?.firstName} ${userData?.lastName}`}
-              src={userData?.avatarUrl}
-            />
-            <Button
-              marginLeft="6"
-              as="a"
-              variant="link"
-              href="https://br.gravatar.com/"
-              target="_blank"
-            >
-              Alterar foto de perfil
-            </Button>
+    <>
+      <HeadUtil title="Minha conta"></HeadUtil>
+      <LoggedAreaLayout>
+        <Title title="Minha conta"></Title>
+        <Container maxW="2xl">
+          <Flex width="full" flexDirection="column" alignItems="center">
+            <Flex width="full" justifyContent="center" alignItems="center">
+              <Avatar
+                size="xl"
+                name={`${userData?.firstName} ${userData?.lastName}`}
+                src={userData?.avatarUrl}
+              />
+              <Button
+                marginLeft="6"
+                as="a"
+                variant="link"
+                href="https://br.gravatar.com/"
+                target="_blank"
+              >
+                Alterar foto de perfil
+              </Button>
+            </Flex>
+            <BasicUserDataForm
+              className={styles["edit-info-form"]}
+            ></BasicUserDataForm>
           </Flex>
-          <BasicUserDataForm
-            className={styles["edit-info-form"]}
-          ></BasicUserDataForm>
-        </Flex>
-      </Container>
-    </LoggedAreaLayout>
+        </Container>
+      </LoggedAreaLayout>
+    </>
   );
 }
