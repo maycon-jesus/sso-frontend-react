53a89d683ec25188ff70ef12d52dc6dd637d8160
Maycon Jesus <email@mayconjesus.dev>
2022-09-18 21:27:15 -0300

    add developer area in drawer

--- src/components/templates/LoggedDrawer.tsx
+++ src/components/templates/LoggedDrawer.tsx
@@ -1,4 +1,9 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
   Button,
   Flex,
@@ -6,18 +11,22 @@ import {
   useDisclosure,
   VStack,
 } from "@chakra-ui/react";
-import { mdiAccount, mdiSecurity } from "@mdi/js";
+import { mdiAccount, mdiApps, mdiSecurity } from "@mdi/js";
 import Icon from "@mdi/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import styles from "./LoggedDrawer.module.scss";
 import classnames from "classnames";
+import { useRecoilValue } from "recoil";
+import { useAuthUserDataState } from "states/Auth";
 
 export default function LoggedDrawer() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const userData = useRecoilValue(useAuthUserDataState);
   useEffect(() => {
     onOpen();
   });
+
   return (
     <div
       className={classnames({
@@ -45,6 +54,7 @@ export default function LoggedDrawer() {
                     as="a"
                     width="full"
                     textAlign="start"
+                    variant="ghost"
                   >
                     Informações Pessoais
                   </Button>
@@ -55,13 +65,43 @@ export default function LoggedDrawer() {
                     as="a"
                     width="full"
                     textAlign="start"
+                    variant="ghost"
                   >
                     Segurança
                   </Button>
                 </Link>
+                <Accordion allowMultiple width="full">
+                  <AccordionItem>
+                    <AccordionButton
+                      padding={0}
+                      _hover={{ bgColor: "transparent" }}
+                    >
+                      <Button
+                        flex={1}
+                        textAlign="start"
+                        variant="ghost"
+                        rightIcon={<AccordionIcon />}
+                      >
+                        Área do Desenvolvedor
+                      </Button>
+                    </AccordionButton>
+                    <AccordionPanel pb={4}>
+                      <Link href="/desenvolvedor/aplicativos" passHref>
+                        <Button
+                          leftIcon={<Icon path={mdiApps} size={1}></Icon>}
+                          as="a"
+                          width="full"
+                          textAlign="start"
+                          variant="ghost"
+                        >
+                          Aplicativos
+                        </Button>
+                      </Link>
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
               </VStack>
             </Box>
-            <Box flexGrow={0}>Teste</Box>
           </Flex>
         </aside>
       </Slide>
